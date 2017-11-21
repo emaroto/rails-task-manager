d90295b72e981ceefb3b667a5d295adb2f00fb4e
Elena Maroto <elena_maroto@hotmail.com>
2017-11-21 15:14:51 +0100

    Task manager done!

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @task = Task.new()
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+
+  def task_params
+    params.require(:task).permit(:name, :due_date)
+  end
+end
